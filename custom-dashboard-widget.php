f1883f77cc62d3ac2f202122a6d8c4e369e722f6
Eyu Kassa <eeyyuuk@gmail.com>
2025-02-05 00:15:03 +0200

    added a few quotes
    Added a few quotes manually until i develop a system that'll get you unlimited motivational quotes like the videos on tiktok you can't reach the end of it no matter how much you scroll.

--- custom-dashboard-widget.php
+++ custom-dashboard-widget.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: EKAZ says hi
- * Description: Adds a friendly widget to the WordPress dashboard.
- * Version: 1.0.0
+ * Plugin Name: Custom Dashboard Widget
+ * Description: Adds a friendly widget to the WordPress dashboard with random motivational quotes.
+ * Version: 1.1.0
  * Author: EKAZ
  */
 
@@ -22,10 +22,27 @@ function custom_widget_content() {
   $post_count = wp_count_posts()->publish;
   $comment_count = wp_count_comments()->approved;
 
+  // Array of random motivational quotes
+  $quotes = [
+    "The only way to do great work is to love what you do. – Steve Jobs",
+    "Believe you can and you're halfway there. – Theodore Roosevelt",
+    "Don't watch the clock; do what it does. Keep going. – Sam Levenson",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. – Winston Churchill",
+    "The best time to plant a tree was 20 years ago. The second best time is now. – Chinese Proverb",
+    "You are never too old to set another goal or to dream a new dream. – C.S. Lewis",
+    "The future belongs to those who believe in the beauty of their dreams. – Eleanor Roosevelt",
+    "Strive not to be a success, but rather to be of value. – Albert Einstein",
+    "The only limit to our realization of tomorrow is our doubts of today. – Franklin D. Roosevelt",
+    "Do what you can, with what you have, where you are. – Theodore Roosevelt"
+  ];
+
+  // Get a random quote
+  $random_quote = $quotes[array_rand($quotes)];
+
   echo "<div class='custom-dashboard-widget'>";
   echo "<p>Hello, <strong>$username</strong>! 🎉</p>";
   echo "<p>Your site has <strong>$post_count posts</strong> and <strong>$comment_count comments</strong>.</p>";
-  echo "<p>Today's Quote: <em>'The only way to do great work is to love what you do.' – Steve Jobs</em></p>";
+  echo "<p>Today's Quote: <em>$random_quote</em></p>";
   echo "</div>";
 }
 
